5cc0254ec9edff3a7e47208b3fb95cc39d66652e
F.Gonçalves <boriscasoy123@gmail.com>
2024-02-28 23:19:53 -0300

    asd

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = require('./src/routes/router')
 const DataBase = require('./src/dataBase/db')
-
+const cors = require('cors')
 
 const app = express()
 const port = 3000
 
-
+app.use()
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
