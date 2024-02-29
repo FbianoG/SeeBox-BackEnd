380e0d17c43b746371c08f40a377745f363b3e7d
F.Gonçalves <boriscasoy123@gmail.com>
2024-02-28 23:07:50 -0300

    sad

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const DataBase = require('./src/dataBase/db')
 
 
 const app = express()
-
 const port = 3000
 
 
+app.use(express.static('public'))
 app.use(express.json())
-
+app.use(express.urlencoded({ extended: true }))
 app.use(router)
 
 
