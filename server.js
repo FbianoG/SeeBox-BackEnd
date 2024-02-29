9bec957768ff31d19984a3a15f9e2259e85cb175
F.Gonçalves <boriscasoy123@gmail.com>
2024-02-28 23:27:22 -0300

    asd

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router = require('./src/routes/router')
+const router = require('./src/routes/router.js')
 const DataBase = require('./src/dataBase/db')
 const cors = require('cors')
 
